ca15103905f80a623afc1500711f82b5c2e88282
taypark <interrupt@kakao.com>
2021-05-25 18:50:01 +0900

    Applied threading but creating thread is a overhead-making job

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -5,30 +5,15 @@ import java.net.Socket;
 
 public class Dispatcher {
 
-    private final int HEADER_SIZE = 6;
-
     public void dispatch(ServerSocket serverSocket, HandleMap handleMap) {
         try {
             Socket socket = serverSocket.accept();
-            demultiplex(socket, handleMap);
+
+            Runnable demultiplexer = new Demultiplexer(socket, handleMap);
+            Thread thread = new Thread(demultiplexer);
+            thread.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public void demultiplex(Socket socket, HandleMap handleMap) {
-        InputStream inputStream = null;
-        byte[] buffer = new byte[HEADER_SIZE];
-
-        try {
-            inputStream = socket.getInputStream();
-            inputStream.read(buffer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        String header = new String(buffer);
-
-        handleMap.get(header).handleEvent(inputStream);
     }
 }
